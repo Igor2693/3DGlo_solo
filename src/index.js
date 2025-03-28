8d33a31c69288517e4396a1e6df8454fc37fc114
Igor Bugaytsov <i.bugaytsov@mail.ru>
2025-03-28 20:54:12 +0300

    Доработка таймера

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 import timer from "./modules/timer";
 
-timer()
+timer('26 may 2025')
--- src/modules/timer.js
+++ src/modules/timer.js
@@ -1,28 +1,33 @@
 
 
-const oneFunc = () => {
+const timer = (deadline) => {
     let hoursTimer = document.getElementById('timer-hours')
     let minutesTimer = document.getElementById('timer-minutes')
     let secondsTimer = document.getElementById('timer-seconds')
+    let daysTimer = document.getElementById('timer-days')
+
 
     let idInterval
     let zero = '0'
 
     const logger = () => {
-        let dedline = new Date('29 march 2025').getTime()
+        let dedline = new Date(deadline).getTime()
         let dateNow = new Date().getTime()
-        let dateTimer = dedline - dateNow
+        let dateTimer = (dedline - dateNow) / 1000
         // console.log(dateTimer);
 
-        let seconds = Math.floor(dateTimer / 1000 % 60)
-        let minutes = Math.floor(dateTimer / 1000 / 60 % 60)
-        let hours = Math.floor(dateTimer / 1000 / 60 / 60 % 60)
+        let seconds = Math.floor(dateTimer % 60)
+        let minutes = Math.floor((dateTimer / 60) % 60)
+        let hours = Math.floor((dateTimer / 60 / 60) % 24)
+        let days = Math.floor((dateTimer / 60 / 60 / 24))
+        // console.log(days);
 
-        secondsTimer.textContent = seconds
-        minutesTimer.textContent = minutes
-        hoursTimer.textContent = hours
-        // console.log(hoursTimer.textContent.length);
-        // console.log(secondsTimer.textContent.length);
+        return { dateTimer, days, hours, minutes, seconds }
+
+        // secondsTimer.textContent = seconds
+        // minutesTimer.textContent = minutes
+        // hoursTimer.textContent = hours
+        // daysTimer.textContent = days
 
         if (secondsTimer.textContent.length === 1) {
             secondsTimer.textContent = zero + seconds
@@ -32,6 +37,9 @@ const oneFunc = () => {
         }
         if (hoursTimer.textContent.length === 1) {
             hoursTimer.textContent = zero + hours
+        }
+        if (daysTimer.textContent.length === 1) {
+            daysTimer.textContent = zero + days
         }
 
         if (dedline < dateNow) {
@@ -43,9 +51,42 @@ const oneFunc = () => {
 
     }
 
-    idInterval = setInterval(logger, 1000)
+
+    const countTimer = () => {
+        const getTime = logger()
+
+        secondsTimer.textContent = getTime.seconds
+        minutesTimer.textContent = getTime.minutes
+        hoursTimer.textContent = getTime.hours
+        daysTimer.textContent = getTime.days
+
+
+        if (secondsTimer.textContent.length === 1) {
+            secondsTimer.textContent = '0' + getTime.seconds
+        }
+        if (minutesTimer.textContent.length === 1) {
+            minutesTimer.textContent = '0' + getTime.minutes
+        }
+        if (hoursTimer.textContent.length === 1) {
+            hoursTimer.textContent = '0' + getTime.hours
+        }
+        if (daysTimer.textContent.length === 1) {
+            daysTimer.textContent = '0' + getTime.days
+        }
+
+        if (getTime.dateTimer < 0) {
+            clearInterval(idInterval)
+            secondsTimer.textContent = '00'
+            minutesTimer.textContent = '00'
+            hoursTimer.textContent = '00'
+        }
+
+        idInterval = setInterval(countTimer, 1000)
+    }
+    countTimer()
+
 
 }
 
 
-export default oneFunc
+export default timer
